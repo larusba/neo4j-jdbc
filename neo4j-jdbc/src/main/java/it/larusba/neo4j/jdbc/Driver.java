2f5ececc7ee354656f5b5362b1986255f3ac410f
Benoît Simard (@logisima) <contact@bsimard.com>
2016-04-11 10:09:46 +0200

    Adding JDBC HTTP Support
    - creation of the new project
    - making some refactoring betweeen parent, bolt and http project
    - first implementation of http driver with http-client

--- neo4j-jdbc/src/main/java/it/larusba/neo4j/jdbc/Driver.java
+++ neo4j-jdbc/src/main/java/it/larusba/neo4j/jdbc/Driver.java
@@ -32,12 +32,21 @@ import java.util.logging.Logger;
  */
 public abstract class Driver implements java.sql.Driver {
 
+	/**
+	 * JDBC prefix for the connection url.
+	 */
+	protected static final String JDBC_PREFIX = "jdbc";
+
+	/**
+	 * Driver perfix for the connection url.
+	 */
+	protected String DRIVER_PREFIX;
+
 	public static String DRIVERS_BOLT = "it.larusba.neo4j.jdbc.bolt.BoltDriver";
-
-	public static String[] DRIVERS = new String[]{DRIVERS_BOLT};
-
+	public static String DRIVERS_HTTP = "it.larusba.neo4j.jdbc.http.HttpDriver";
+	public static String[] DRIVERS = new String[] { DRIVERS_BOLT, DRIVERS_HTTP };
 	static {
-		for(String driver : DRIVERS) {
+		for (String driver : DRIVERS) {
 			try {
 				Class.forName(driver);
 			} catch (ClassNotFoundException e) {
@@ -45,9 +54,16 @@ public abstract class Driver implements java.sql.Driver {
 		}
 	}
 
-	@Override public abstract Connection connect(String url, Properties info) throws SQLException;
+	/**
+	 * Default constructor.
+	 *
+	 * @param prefix Prefix of the driver for the connection url.
+	 */
+	protected Driver(String prefix) {
+		this.DRIVER_PREFIX = prefix;
+	}
 
-	@Override public abstract boolean acceptsURL(String url) throws SQLException;
+	@Override public abstract Connection connect(String url, Properties info) throws SQLException;
 
 	@Override public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
 		return new DriverPropertyInfo[0];
@@ -67,5 +83,42 @@ public abstract class Driver implements java.sql.Driver {
 
 	@Override public Logger getParentLogger() throws SQLFeatureNotSupportedException {
 		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+
+	@Override public boolean acceptsURL(String url) throws SQLException {
+		if (url == null) {
+			throw new SQLException("null is not a valid url");
+		}
+		String[] pieces = url.split(":");
+		if (pieces.length > 2) {
+			if (JDBC_PREFIX.equals(pieces[0]) && DRIVER_PREFIX.equals(pieces[1])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Parse the url string and construct a properties object.
+	 *
+	 * @param url The url to parse
+	 * @param properties The properties
+	 */
+	protected Properties parseUrlProperties(String url, Properties properties) {
+		if (url.contains("?")) {
+			String urlProps = url.substring(url.indexOf('?') + 1);
+			String[] props = urlProps.split(",");
+			for (String prop : props) {
+				int idx = prop.indexOf('=');
+				if (idx != -1) {
+					String key = prop.substring(0, idx);
+					String value = prop.substring(idx + 1);
+					properties.put(key, value);
+				} else {
+					properties.put(prop, "true");
+				}
+			}
+		}
+		return properties;
 	}
 }
